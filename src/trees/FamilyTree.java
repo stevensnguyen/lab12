afa80450de6b3b9e3e383e478e0451862d795c15
stevensnguyen <stevensinguyen@gmail.com>
2022-11-18 09:14:08 -0800

    Updated TreeNode class

--- src/trees/FamilyTree.java
+++ src/trees/FamilyTree.java
@@ -33,6 +33,8 @@ public class FamilyTree
         {
             // Add childNode to this node's children list. Also
             // set childNode's parent to this node.
+        	children.add(childNode);
+        	parent = childNode;
         }
         
         
@@ -41,7 +43,7 @@ public class FamilyTree
         TreeNode getNodeWithName(String targetName)
         {
             // Does this node have the target name?
-            if (?????)
+            if (targetName == getName())
                 return this;
                     
             // No, recurse. Check all children of this node.
@@ -49,6 +51,9 @@ public class FamilyTree
             {
                 // If child.getNodeWithName(targetName) returns a non-null node,
                 // then that's the node we're looking for. Return it.
+            	if(child.getNodeWithName(targetName) != null) {
+            		return child.getNodeWithName(targetName);
+            	}
             }
             
             // Not found anywhere.
@@ -61,12 +66,23 @@ public class FamilyTree
         ArrayList<TreeNode> collectAncestorsToList()
         {
             ArrayList<TreeNode> ancestors = new ArrayList<>();
-
+            TreeNode current = parent;
             // ?????  Collect ancestors of this TreeNode into the array list. HINT: going up
             // the nodes of a tree is like traversing a linked list. If that isn’t clear,
             // draw a tree, mark any leaf node, and then mark its ancestors in order from
             // recent to ancient. Expect a question about this on the final exam.
-
+            	while(current.parent != null) {
+            		for(int i = 0; i < current.children.size(); i++) {
+            			ancestors.add(current.children.get(i));
+            			current = parent;
+            		}
+              
+            	}
+            	if (current.parent == null) {
+            	ancestors.add(current);	
+            	}
+            	
+        
             return ancestors;
         }
         
